5fdc8a8a6a9bcd901ddb044f4bd6aeab9fe09ed2
Scott Lawrence <bytbox@gmail.com>
2011-12-30 03:28:57 -0500

    Monad works

--- monads/main.go
+++ monads/main.go
@@ -4,6 +4,11 @@ type Functor interface {
 	Fmap(func(interface{}) interface{})
 }
 
+type Monad interface {
+	Sequence(func(interface{}) Monad) Monad
+	Return(interface{})
+}
+
 type List struct {
 	data []interface{}
 }
@@ -14,14 +19,26 @@ func NewList() *List {
 	}
 }
 
-func (l *List) Append(v interface{}) {
-	l.data = append(l.data, v)
+func (l *List) Append(v ...interface{}) {
+	l.data = append(l.data, v...)
 }
 
 func (l *List) Fmap(f func(interface{}) interface{}) {
 	for i, v := range l.data {
 		l.data[i] = f(v)
 	}
+}
+
+func (l *List) Return(v interface{}) {
+	l.data = []interface{}{v}
+}
+
+func (l *List) Sequence(f func(interface{}) Monad) Monad {
+	r := NewList()
+	for _, v := range l.data {
+		r.Append(f(v).(*List).data...)
+	}
+	return r
 }
 
 // Yup, it's an ugly hack
@@ -33,8 +50,11 @@ func (l *List) PrintInts() {
 
 func main() {
 	l := NewList()
-	l.Append(1)
-	l.Append(2)
-	l.Append(3)
-	l.PrintInts()
+	l.Append(1, 2, 3)
+	l.Sequence(func(v interface{}) Monad {
+		l := NewList()
+		l.Return(v.(int)*5)
+		l.Append(v.(int)*7)
+		return l
+	}).(*List).PrintInts()
 }
